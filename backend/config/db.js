16c70f50526fde1bc5916f813eece650c3bb8fe2
Mansi231 <mansigabani231@gmail.com>
2023-12-12 17:51:47 +0530

    mongo uri password change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // connect to db 
 
-const db = mongoose.connect("mongodb+srv://mansigabani231:vwG5xJv9WG3vZgZ5@mern-chat-app.rymtaoj.mongodb.net/?retryWrites=true&w=majority").then(() => {
+const db = mongoose.connect("mongodb+srv://mansigabani231:ndQgnWcglyAIYnSk@mern-chat-app.rymtaoj.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log('connection successfull')
  }).catch((err) => { console.log(err,'-----failed connection') })
 
